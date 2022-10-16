dc3a908bec47112d3c643e57231fa2c58099f369
Giuseppe Stelluto <giuseppe@gstelluto.com>
2022-10-16 13:51:57 -0400

    Print basic info about games in the collection

--- main.py
+++ main.py
@@ -2,63 +2,49 @@
 import requests
 import xml.etree.ElementTree as ET
 import os
-
 import logging as LOGGER
-LOGGER.basicConfig(level=LOGGER.DEBUG)
+from boardgamegeek import BGGClient
+import boardgamegeek as bggm
+
+LOGGER.basicConfig(level=LOGGER.WARNING)
 # LOGGER = logging.getLogger('main')
 
 USERNAME="ColbyBoardgames"
-BASEURL="https://boardgamegeek.com/xmlapi2/"
-CACHEFILE='cached-res.xml'
 
-def get_game_info(id):
-    r = requests.get(f"{BASEURL}thing?id={USERNAME}")
-    if r:
-        xmlstring = r.text
-        print(xmlstring)
-        tree = ET.fromstring(xmlstring)
-        LOGGER.debug(tree.tag)
-        LOGGER.debug(tree.attrib)
-        for child in tree:
-            LOGGER.debug(child.tag)
-            LOGGER.debug(child.attrib)
-            LOGGER.debug('============')
-    
-
-def get_games_ids(xmlstring):
-    tree = ET.fromstring(xmlstring)
-    root = tree
-        # LOGGER.debug(f"{child.tag} : {child.attrib}")
-    ids = [child.attrib.get('objectid') for child in root]
-    return ids
+bgg = BGGClient()
 
 
 def main():
     """Program entrypoint
     :returns: TODO
-
     """
-    xmlstring = ''
-    if os.path.exists(CACHEFILE):
-        with open(CACHEFILE) as f:
-            LOGGER.debug('Response cached:')
-            xmlstring = f.read()
-            # LOGGER.debug(xmlstring)
-    
-    if not xmlstring:
-        # r = requests.get(BASEURL+"user?name=MeeplesAndMorselsClub")
-        r = requests.get(f"{BASEURL}collection?username={USERNAME}")
-        if r:
-            LOGGER.debug(r)
-            xmlstring = r.text
-            with open(CACHEFILE, 'w') as f:
-                LOGGER.debug('Caching response')
-                LOGGER.debug(xmlstring)
-                f.write(xmlstring)
-    ids = get_games_ids(xmlstring)
-    for id in ids[:1]:
-        get_game_info(id)
-    return ids
+    game_collection = bgg.collection(
+        USERNAME,
+        exclude_subtype='boardgameexpansion')
+        # exclude_subtype=[bggm.utils.BGGRestrictCollectionTo.BOARD_GAME_EXTENSION])
+    game_ids = [game.id for game in game_collection.items]
+    games = bgg.game_list(game_id_list=game_ids)
+    for game in games:
+        print('')
+        print("====== " + game.name + " " + "="*(80-len(game.name)))
+        print(f"Weight: {game.rating_average_weight}")
+        data = game.data()
+        minplayers = data['minplayers']
+        maxplayers = data['maxplayers']
+        if minplayers == maxplayers:
+            player_range = minplayers
+        else:
+            player_range = f"{minplayers} - {maxplayers}"
+        rating = data['stats']['average']
+        rank = -1
+        for rankinfo in data['stats']['ranks']:
+            if rankinfo['id'] == '1':
+                rank = rankinfo['value']
+                break
+        print(f"Rank: {rank}")
+        print(f"User Rating: {rating}")
+        print(f"Players: {player_range}")
+
 
 if __name__ == "__main__":
     main()
